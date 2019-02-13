294717f0b9342e5347694077a719305c82a5f77d
mharder@msft <mhardermicrosoft@MININT-TA2USRP.redmond.corp.microsoft.com>
2019-02-12 22:52:43 -0800

    Test HttpRequestMessage.Version

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AzurePipelineTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"HttpRequestMessage.Version: {(new HttpRequestMessage()).Version}");
+        }
+    }
+}
