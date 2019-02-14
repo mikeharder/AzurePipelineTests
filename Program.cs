a2d129c7dad728b29e11df607f0e75adc89d7228
Mike Harder <mharder@microsoft.com>
2019-02-14 15:17:04 -0800

    Print more info

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace AzurePipelineTests
     {
         static void Main(string[] args)
         {
+            var version = (new HttpRequestMessage()).Version.ToString();
+            var path = typeof(object).Assembly.Location;
+            Assert.Fail($"Path: {path}, Version: {version}");
             Console.WriteLine($"HttpRequestMessage.Version: {(new HttpRequestMessage()).Version}");
         }
     }
