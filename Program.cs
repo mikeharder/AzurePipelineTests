0f2d6938644bf7825d4bc181ec868a60e1411a2c
Mike Harder <mharder@microsoft.com>
2019-02-22 14:28:10 -0800

    Write to console from .net app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace AzurePipelineTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("before");
+            Thread.Sleep(30 * 1000);
+            Console.WriteLine("after");
+        }
+    }
+}
