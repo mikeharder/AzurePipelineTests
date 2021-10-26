9137ecdb2b2c76fb06031418311b3f5e1fdbe41f
Mike Harder <mharder@microsoft.com>
2021-10-26 16:29:59 -0700

    test venv

--- net-test/Program.cs
+++ net-test/Program.cs
@@ -16,44 +16,51 @@ namespace net_test
             Console.WriteLine("--- Error ---");
             Console.WriteLine(result.StandardError);
 
-            var process = new Process() {
-                StartInfo = {
-                    FileName = "python3",
-                    Arguments = "--version",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
-            
-            var outputBuilder = new StringBuilder();
-            process.OutputDataReceived += (_, e) => {
-                if (e.Data != null) {
-                    outputBuilder.AppendLine(e.Data);
-                }
-            };
-
-            var errorBuilder = new StringBuilder();
-            process.ErrorDataReceived += (_, e) => {
-                if (e.Data != null) {
-                    errorBuilder.AppendLine(e.Data);
-                }
-            };
-
-            process.Start();
-
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-
-            process.WaitForExit();
-
+            result = await Util.RunAsync("python3", "-m venv env-perf", ".", throwOnError: true);
             Console.WriteLine("--- Output ---");
-            Console.WriteLine(outputBuilder.ToString());
+            Console.WriteLine(result.StandardOutput);
             Console.WriteLine("--- Error ---");
-            Console.WriteLine(errorBuilder.ToString());
+            Console.WriteLine(result.StandardError);
 
-            Console.WriteLine("done");
-        }
+
+        //     var process = new Process() {
+        //         StartInfo = {
+        //             FileName = "python3",
+        //             Arguments = "--version",
+        //             RedirectStandardOutput = true,
+        //             RedirectStandardError = true,
+        //             UseShellExecute = false,
+        //             CreateNoWindow = true,
+        //         }
+        //     };
+            
+        //     var outputBuilder = new StringBuilder();
+        //     process.OutputDataReceived += (_, e) => {
+        //         if (e.Data != null) {
+        //             outputBuilder.AppendLine(e.Data);
+        //         }
+        //     };
+
+        //     var errorBuilder = new StringBuilder();
+        //     process.ErrorDataReceived += (_, e) => {
+        //         if (e.Data != null) {
+        //             errorBuilder.AppendLine(e.Data);
+        //         }
+        //     };
+
+        //     process.Start();
+
+        //     process.BeginOutputReadLine();
+        //     process.BeginErrorReadLine();
+
+        //     process.WaitForExit();
+
+        //     Console.WriteLine("--- Output ---");
+        //     Console.WriteLine(outputBuilder.ToString());
+        //     Console.WriteLine("--- Error ---");
+        //     Console.WriteLine(errorBuilder.ToString());
+
+        //     Console.WriteLine("done");
+        // }
     }
 }
