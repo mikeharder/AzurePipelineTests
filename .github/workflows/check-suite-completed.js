3578404400d695687e235808266e6a4378647b7f
Mike Harder <mharder@microsoft.com>
2025-01-23 22:06:57 +0000

    list all suites

--- .github/workflows/check-suite-completed.js
+++ .github/workflows/check-suite-completed.js
@@ -13,14 +13,27 @@ module.exports = async ({ github, context, core }) => {
         context.payload
       );
 
-    const checkRuns = await github.rest.checks.listForSuite({
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      check_suite_id: payload.check_suite.id,
+    const owner = payload.repository.owner.login;
+    const repo = payload.repository.name;
+
+    const checkSuites = await github.rest.checks.listSuitesForRef({
+      owner: owner,
+      repo: repo,
+      ref: payload.check_suite.head_sha,
     });
 
-    for (const run of checkRuns.data.check_runs) {
-      console.log(JSON.stringify(run));
+    for (const suite of checkSuites.data.check_suites) {
+      console.log(JSON.stringify(suite));
+
+      const checkRuns = await github.rest.checks.listForSuite({
+        owner: owner,
+        repo: repo,
+        check_suite_id: suite.id,
+      });
+
+      for (const run of checkRuns.data.check_runs) {
+        console.log(JSON.stringify(run));
+      }
     }
   }
 };
