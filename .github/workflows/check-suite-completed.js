ab4d2e1e1bac9570d1eea63c99d3d98c26e8a780
Mike Harder <mharder@microsoft.com>
2025-01-24 04:14:32 +0000

    fix workflow args

--- .github/workflows/check-suite-completed.js
+++ .github/workflows/check-suite-completed.js
@@ -4,37 +4,38 @@
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
  */
 module.exports = async ({ github, context, core }) => {
-  if (
-    context.eventName === 'check_suite' &&
-    context.payload.action == 'completed'
-  ) {
+  let owner = process.env.OWNER || '';
+  let repo = process.env.REPO || '';
+  let head_sha = process.env.HEAD_SHA || '';
+
+  if (context.eventName === 'check_suite') {
     const payload =
       /** @type {import("@octokit/webhooks-types").CheckSuiteCompletedEvent} */ (
         context.payload
       );
 
-    let owner = process.env.OWNER || payload.repository.owner.login;
-    let repo = process.env.REPO || payload.repository.name;
-    let head_sha = process.env.HEAD_SHA || payload.check_suite.head_sha;
+    owner = owner || payload.repository.owner.login;
+    repo = repo || payload.repository.name;
+    head_sha = head_sha || payload.check_suite.head_sha;
+  }
 
-    const checkSuites = await github.rest.checks.listSuitesForRef({
+  const checkSuites = await github.rest.checks.listSuitesForRef({
+    owner: owner,
+    repo: repo,
+    ref: head_sha,
+  });
+
+  for (const suite of checkSuites.data.check_suites) {
+    console.log(JSON.stringify(suite));
+
+    const checkRuns = await github.rest.checks.listForSuite({
       owner: owner,
       repo: repo,
-      ref: head_sha,
+      check_suite_id: suite.id,
     });
 
-    for (const suite of checkSuites.data.check_suites) {
-      console.log(JSON.stringify(suite));
-
-      const checkRuns = await github.rest.checks.listForSuite({
-        owner: owner,
-        repo: repo,
-        check_suite_id: suite.id,
-      });
-
-      for (const run of checkRuns.data.check_runs) {
-        console.log(JSON.stringify(run));
-      }
+    for (const run of checkRuns.data.check_runs) {
+      console.log(JSON.stringify(run));
     }
   }
 };
