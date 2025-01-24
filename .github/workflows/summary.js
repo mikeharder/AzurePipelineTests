5a1662e40690e8d0766baacb73fac3d9e517d3b4
Mike Harder <mharder@microsoft.com>
2025-01-24 07:32:30 +0000

    Add business logic

--- .github/workflows/summary.js
+++ .github/workflows/summary.js
@@ -9,6 +9,7 @@ module.exports = async ({ github, context, core }) => {
   let issue_number = parseInt(process.env.ISSUE_NUMBER || '');
   let head_sha = process.env.HEAD_SHA || '';
 
+  //#region Extract inputs from context
   if (context.eventName === 'check_suite') {
     const payload =
       /** @type {import("@octokit/webhooks-types").CheckSuiteEvent} */ (
@@ -44,25 +45,39 @@ module.exports = async ({ github, context, core }) => {
     // TODO: May not work in fork PRs, manually triggered check suites, etc
     issue_number = issue_number || payload.workflow_run.pull_requests[0].number;
   }
+  //#endregion
 
+  const labels = (
+    await github.rest.issues.listLabelsOnIssue({
+      owner: owner,
+      repo: repo,
+      issue_number: issue_number,
+    })
+  ).data.map((label) => label.name);
+  const checkSuites = (
+    await github.rest.checks.listSuitesForRef({
+      owner: owner,
+      repo: repo,
+      ref: head_sha,
+    })
+  ).data.check_suites;
+  const checkRuns = (
+    await github.rest.checks.listForRef({
+      owner: owner,
+      repo: repo,
+      ref: head_sha,
+    })
+  ).data.check_runs;
+
+  //#region Log Labels, Check Suites, and Check Runs
   console.log('# Labels');
-  const labels = await github.rest.issues.listLabelsOnIssue({
-    owner: owner,
-    repo: repo,
-    issue_number: issue_number,
-  });
-  for (const label of labels.data) {
-    console.log(`  ${label.name}`);
+  for (const label of labels) {
+    console.log(`  ${label}`);
   }
 
   console.log();
   console.log('# Check Suites');
-  const checkSuites = await github.rest.checks.listSuitesForRef({
-    owner: owner,
-    repo: repo,
-    ref: head_sha,
-  });
-  for (const suite of checkSuites.data.check_suites) {
+  for (const suite of checkSuites) {
     console.log(`${suite.app?.name}: ${suite.status}, ${suite.conclusion}`);
 
     const checkRuns = await github.rest.checks.listForSuite({
@@ -78,12 +93,29 @@ module.exports = async ({ github, context, core }) => {
 
   console.log();
   console.log('# Check Runs');
-  const checkRuns = await github.rest.checks.listForRef({
-    owner: owner,
-    repo: repo,
-    ref: head_sha,
-  });
-  for (const run of checkRuns.data.check_runs) {
+  for (const run of checkRuns) {
     console.log(`  ${run.name}: ${run.status}, ${run.conclusion}`);
   }
+  //#endregion
+
+  const swaggerLintDiffSucceeded = checkRuns.some(
+    (run) =>
+      run.name === 'Swagger LintDiff' &&
+      run.status === 'completed' &&
+      run.conclusion === 'success',
+  );
+
+  const allLabelsMatch =
+    labels.includes('ARMReview') &&
+    !labels.includes('NotReadyForARMReview') &&
+    labels.includes('ARMReview') &&
+    labels.includes('ARMBestPractices') &&
+    labels.includes('rp-service-existing') &&
+    labels.includes('typespec-incremental') &&
+    (!labels.includes('SuppressionReviewRequired') ||
+      labels.includes('Suppression-Approved'));
+
+  const armAutomatedSignOff = swaggerLintDiffSucceeded && allLabelsMatch;
+
+  console.log(`armAutomatedSignOff: ${armAutomatedSignOff}`);
 };
