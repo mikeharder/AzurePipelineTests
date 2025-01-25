ac9f873598674d18192fa4903fdccf3329f889e5
Mike Harder <mharder@microsoft.com>
2025-01-25 01:55:01 +0000

    Remove unused code

--- .github/workflows/src/summary.js
+++ .github/workflows/src/summary.js
@@ -32,18 +32,6 @@ module.exports = async ({ github, context, core }) => {
     repo = repo || payload.repository.name;
     head_sha = head_sha || payload.pull_request.head.sha;
     issue_number = issue_number || payload.pull_request.number;
-  } else if (context.eventName === 'workflow_run') {
-    const payload =
-      /** @type {import("@octokit/webhooks-types").WorkflowRunEvent} */ (
-        context.payload
-      );
-
-    owner = owner || payload.repository.owner.login;
-    repo = repo || payload.repository.name;
-    head_sha = head_sha || payload.workflow_run.head_sha;
-
-    // TODO: May not work in fork PRs, manually triggered check suites, etc
-    issue_number = issue_number || payload.workflow_run.pull_requests[0].number;
   }
   //#endregion
 
