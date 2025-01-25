cc3d883b92ddc9269e35459459c3001782fb5f99
Mike Harder <mharder@microsoft.com>
2025-01-25 01:44:20 +0000

    Refactor

--- .github/workflows/test/summary.test.js
+++ .github/workflows/test/summary.test.js
@@ -1,36 +1,43 @@
 import { describe, it, vi } from 'vitest';
 import summary from '../src/summary';
 
+function createMockGithub() {
+  return {
+    rest: {
+      issues: {
+        listLabelsOnIssue: vi.fn().mockResolvedValue({
+          data: [],
+        }),
+        removeLabel: vi.fn().mockResolvedValue(),
+      },
+    },
+  };
+}
+
+function createMockContextPullRequest() {
+  return {
+    eventName: 'pull_request',
+    payload: {
+      repository: {
+        name: 'TestRepoName',
+        owner: {
+          login: 'TestRepoOwnerLogin',
+        },
+      },
+      pull_request: {
+        head: {
+          sha: 'abc123',
+        },
+        number: 123,
+      },
+    },
+  };
+}
+
 describe('summary', () => {
   it('removes ArmAutoSignOff if labels do not match', async ({ expect }) => {
-    const github = {
-      rest: {
-        issues: {
-          listLabelsOnIssue: vi.fn().mockResolvedValue({
-            data: [{ name: 'TestLabel' }],
-          }),
-          removeLabel: vi.fn().mockResolvedValue(),
-        },
-      },
-    };
-
-    const context = {
-      eventName: 'pull_request',
-      payload: {
-        repository: {
-          name: 'TestRepoName',
-          owner: {
-            login: 'TestRepoOwnerLogin',
-          },
-        },
-        pull_request: {
-          head: {
-            sha: 'abc123',
-          },
-          number: 123,
-        },
-      },
-    };
+    const github = createMockGithub();
+    const context = createMockContextPullRequest();
 
     await summary({ github, context, core: undefined });
 
