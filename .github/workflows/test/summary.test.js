4a2aa00dbfd1caa612b970f93a89cda31e24e5d9
Mike Harder <mharder@microsoft.com>
2025-01-25 01:25:23 +0000

    Convert to describe/it

--- .github/workflows/test/summary.test.js
+++ .github/workflows/test/summary.test.js
@@ -1,38 +1,40 @@
-import { test } from 'vitest';
+import { describe, it } from 'vitest';
 import summary from '../src/summary';
 
-test('check_suite', async ({ expect }) => {
-  const github = {
-    rest: {
-      issues: {
-        listLabelsOnIssue: () =>
-          Promise.resolve({
-            data: [{ name: 'TestLabel' }],
-          }),
-        removeLabel: () => Promise.resolve(),
-      },
-    },
-  };
-
-  const context = {
-    eventName: 'check_suite',
-    payload: {
-      repository: {
-        name: 'TestRepoName',
-        owner: {
-          login: 'TestRepoOwnerLogin',
+describe('summary', () => {
+  it('check_suite', async ({ expect }) => {
+    const github = {
+      rest: {
+        issues: {
+          listLabelsOnIssue: () =>
+            Promise.resolve({
+              data: [{ name: 'TestLabel' }],
+            }),
+          removeLabel: () => Promise.resolve(),
         },
       },
-      check_suite: {
-        head_sha: 'abc123',
-        pull_requests: [
-          {
-            number: 123,
-          },
-        ],
-      },
-    },
-  };
+    };
 
-  await summary({ github: github, context: context, core: null });
+    const context = {
+      eventName: 'check_suite',
+      payload: {
+        repository: {
+          name: 'TestRepoName',
+          owner: {
+            login: 'TestRepoOwnerLogin',
+          },
+        },
+        check_suite: {
+          head_sha: 'abc123',
+          pull_requests: [
+            {
+              number: 123,
+            },
+          ],
+        },
+      },
+    };
+
+    await summary({ github: github, context: context, core: null });
+  });
 });
