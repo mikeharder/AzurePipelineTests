5128bffef8ff0f8ff971569e2c56b47287de1f67
Mike Harder <mharder@microsoft.com>
2025-01-28 19:29:30 +0000

    Use require for internal imports

--- .github/workflows/test/summary.test.js
+++ .github/workflows/test/summary.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
-import summary from '../src/summary';
+const summary = require('../src/summary');
 
 describe('summary', () => {
   it('loads inputs from env', async () => {
