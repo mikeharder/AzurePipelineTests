a7b37b26375984481739528ee79eb6d4a185f2b4
Mike Harder <mharder@microsoft.com>
2025-01-28 02:20:37 +0000

    Simplify tests

--- .github/workflows/test/summary.test.js
+++ .github/workflows/test/summary.test.js
@@ -32,7 +32,7 @@ describe('summary', () => {
     });
   });
 
-  describe('removes label if labels not match', () => {
+  describe('extracts input from context', () => {
     it.each([
       ['check_suite', createMockContextCheckSuite()],
       ['pull_request', createMockContextPullRequest()],
@@ -51,140 +51,136 @@ describe('summary', () => {
     });
   });
 
-  describe('adds and removes label based on check status', () => {
-    it.each([
-      ['check_suite', createMockContextCheckSuite()],
-      ['pull_request', createMockContextPullRequest()],
-    ])('%s', async (_, context) => {
-      const github = createMockGithub();
-      github.rest.issues.listLabelsOnIssue.mockResolvedValue({
-        data: [
-          { name: 'ARMReview' },
-          { name: 'ARMBestPractices' },
-          { name: 'rp-service-existing' },
-          { name: 'typespec-incremental' },
-          { name: 'SuppressionReviewRequired' },
-          { name: 'Suppression-Approved' },
-        ],
-      });
-
-      const core = createMockCore();
-
-      // Before check starts running
-      await summary({ github, context, core });
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
-
-      // Check in-progress
-      github.rest.checks.listForRef.mockResolvedValue({
-        data: {
-          check_runs: [
-            {
-              name: 'Swagger LintDiff',
-              status: 'in_progress',
-              conclusion: null,
-            },
-          ],
-        },
-      });
-      await summary({ github, context, core });
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
-
-      // Check completed with success
-      github.rest.checks.listForRef.mockResolvedValue({
-        data: {
-          check_runs: [
-            {
-              name: 'Swagger LintDiff',
-              status: 'completed',
-              conclusion: 'success',
-            },
-          ],
-        },
-      });
-      await summary({ github, context, core });
-      expect(github.rest.issues.addLabels).toHaveBeenCalledWith({
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: 123,
-        labels: ['ARMAutomatedSignOff'],
-      });
-      expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
-
-      // Check completed with failure
-      github.rest.issues.addLabels.mockReset();
-      github.rest.issues.removeLabel.mockReset();
-      github.rest.checks.listForRef.mockResolvedValue({
-        data: {
-          check_runs: [
-            {
-              name: 'Swagger LintDiff',
-              status: 'completed',
-              conclusion: 'failure',
-            },
-          ],
-        },
-      });
-      await summary({ github, context, core });
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: 123,
-        name: 'ARMAutomatedSignOff',
-      });
-
-      // Ignore 404 from removeLabel()
-      github.rest.issues.addLabels.mockReset();
-      github.rest.issues.removeLabel.mockReset();
-      github.rest.issues.removeLabel.mockRejectedValue({ status: 404 });
-      await summary({ github, context, core });
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: 123,
-        name: 'ARMAutomatedSignOff',
-      });
-
-      // Throw if other error from removeLabel()
-      github.rest.issues.addLabels.mockReset();
-      github.rest.issues.removeLabel.mockReset();
-      github.rest.issues.removeLabel.mockRejectedValue({ status: 500 });
-      await expect(summary({ github, context, core })).rejects.toThrow();
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: 123,
-        name: 'ARMAutomatedSignOff',
-      });
-
-      // Invalid: multiple check runs named "Swagger LintDiff"
-      github.rest.issues.addLabels.mockReset();
-      github.rest.issues.removeLabel.mockReset();
-      github.rest.checks.listForRef.mockResolvedValue({
-        data: {
-          check_runs: [
-            {
-              name: 'Swagger LintDiff',
-              status: 'completed',
-              conclusion: 'success',
-            },
-            {
-              name: 'Swagger LintDiff',
-              status: 'completed',
-              conclusion: 'failure',
-            },
-          ],
-        },
-      });
-
-      await expect(summary({ github, context, core })).rejects.toThrow();
-      expect(github.rest.issues.addLabels).toBeCalledTimes(0);
-      expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
+  it('adds and removes label based on check status', async () => {
+    const github = createMockGithub();
+    github.rest.issues.listLabelsOnIssue.mockResolvedValue({
+      data: [
+        { name: 'ARMReview' },
+        { name: 'ARMBestPractices' },
+        { name: 'rp-service-existing' },
+        { name: 'typespec-incremental' },
+        { name: 'SuppressionReviewRequired' },
+        { name: 'Suppression-Approved' },
+      ],
     });
+
+    const context = createMockContextCheckSuite();
+    const core = createMockCore();
+
+    // Before check starts running
+    await summary({ github, context, core });
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
+
+    // Check in-progress
+    github.rest.checks.listForRef.mockResolvedValue({
+      data: {
+        check_runs: [
+          {
+            name: 'Swagger LintDiff',
+            status: 'in_progress',
+            conclusion: null,
+          },
+        ],
+      },
+    });
+    await summary({ github, context, core });
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
+
+    // Check completed with success
+    github.rest.checks.listForRef.mockResolvedValue({
+      data: {
+        check_runs: [
+          {
+            name: 'Swagger LintDiff',
+            status: 'completed',
+            conclusion: 'success',
+          },
+        ],
+      },
+    });
+    await summary({ github, context, core });
+    expect(github.rest.issues.addLabels).toHaveBeenCalledWith({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: 123,
+      labels: ['ARMAutomatedSignOff'],
+    });
+    expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
+
+    // Check completed with failure
+    github.rest.issues.addLabels.mockReset();
+    github.rest.issues.removeLabel.mockReset();
+    github.rest.checks.listForRef.mockResolvedValue({
+      data: {
+        check_runs: [
+          {
+            name: 'Swagger LintDiff',
+            status: 'completed',
+            conclusion: 'failure',
+          },
+        ],
+      },
+    });
+    await summary({ github, context, core });
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: 123,
+      name: 'ARMAutomatedSignOff',
+    });
+
+    // Ignore 404 from removeLabel()
+    github.rest.issues.addLabels.mockReset();
+    github.rest.issues.removeLabel.mockReset();
+    github.rest.issues.removeLabel.mockRejectedValue({ status: 404 });
+    await summary({ github, context, core });
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: 123,
+      name: 'ARMAutomatedSignOff',
+    });
+
+    // Throw if other error from removeLabel()
+    github.rest.issues.addLabels.mockReset();
+    github.rest.issues.removeLabel.mockReset();
+    github.rest.issues.removeLabel.mockRejectedValue({ status: 500 });
+    await expect(summary({ github, context, core })).rejects.toThrow();
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toHaveBeenCalledWith({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      issue_number: 123,
+      name: 'ARMAutomatedSignOff',
+    });
+
+    // Invalid: multiple check runs named "Swagger LintDiff"
+    github.rest.issues.addLabels.mockReset();
+    github.rest.issues.removeLabel.mockReset();
+    github.rest.checks.listForRef.mockResolvedValue({
+      data: {
+        check_runs: [
+          {
+            name: 'Swagger LintDiff',
+            status: 'completed',
+            conclusion: 'success',
+          },
+          {
+            name: 'Swagger LintDiff',
+            status: 'completed',
+            conclusion: 'failure',
+          },
+        ],
+      },
+    });
+
+    await expect(summary({ github, context, core })).rejects.toThrow();
+    expect(github.rest.issues.addLabels).toBeCalledTimes(0);
+    expect(github.rest.issues.removeLabel).toBeCalledTimes(0);
   });
 });
 
