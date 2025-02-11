9c2e507e0a002966204b69c31763aaa7c5d1554a
Mike Harder <mharder@microsoft.com>
2025-02-11 22:08:51 +0000

    print check status from nstm

---
+++ .github/workflows/src/ntsm-comment.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import { extractInputs } from '../../src/context.js';
+
+/**
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ */
+export default async function ntsmComment({ github, context, core }) {
+  let inputs = await extractInputs(github, context, core);
+
+  // Get all check runs for the PR
+  const checkRuns = await github.rest.checks.listForRef({
+    owner: inputs.owner,
+    repo: inputs.repo,
+    ref: inputs.head_sha,
+  });
+
+  console.log('Check Runs:');
+  checkRuns.data.check_runs.forEach((check) => {
+    console.log(`- ${check.name}: ${check.conclusion || check.status}`);
+  });
+}
