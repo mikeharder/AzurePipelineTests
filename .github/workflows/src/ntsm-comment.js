f2c6623f16373ffae98fee6f566152ef24cab4ef
Mike Harder <mharder@microsoft.com>
2025-02-12 21:30:27 +0000

    print workflow runs

--- .github/workflows/src/ntsm-comment.js
+++ .github/workflows/src/ntsm-comment.js
@@ -6,13 +6,28 @@ import { extractInputs } from '../../src/context.js';
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
  */
 export default async function ntsmComment({ github, context, core }) {
-  let inputs = await extractInputs(github, context, core);
+  let { repo, owner, head_sha } = await extractInputs(github, context, core);
+
+  const workflowRuns = await github.rest.actions.listWorkflowRunsForRepo({
+    repo,
+    owner,
+    // event: 'pull_request',
+    // status: 'completed',
+    per_page: 100,
+    head_sha,
+  });
+
+  console.log('Workflow Runs:');
+  workflowRuns.data.workflow_runs.forEach((wf) => {
+    console.log(`- ${wf.name}: ${wf.conclusion || wf.status}`);
+    console.log(JSON.stringify(wf));
+  });
 
   // Get all check runs for the PR
   const checkRuns = await github.rest.checks.listForRef({
-    owner: inputs.owner,
-    repo: inputs.repo,
-    ref: inputs.head_sha,
+    owner,
+    repo,
+    ref: head_sha,
   });
 
   console.log('Check Runs:');
